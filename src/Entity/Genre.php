c0c7e4f9a32bfa7b975ef90193b4d0e6a5ff1dfb
Robin Simonneau <robin.simonneau@etudiant.univ-reims.fr>
2022-05-30 16:15:14 +0200

    Ajout de la class Genre et de getters

---
+++ src/Entity/Genre.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Entity;
+
+class Genre
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * Getter de l'id
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter du nom du genre
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}

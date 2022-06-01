1bb0c322a8594fcf03dba64c069ccce8d3c3f14f
Robin Simonneau <robin.simonneau@etudiant.univ-reims.fr>
2022-06-01 13:15:13 +0200

    Problème de redirection réglé, et début de mis en forme de l'affichage des saisons

--- public/season.php
+++ public/season.php
@@ -19,28 +19,31 @@ try {
     $string = AppWebPage::escapeString($tvshow->getName());
     $webPage->setTitle("Serie TV : $string");
 
-    $webPage->appendContent("<div class=tvshow>");
-    $webPage->appendContent("<div class='poster_serie'><img src='poster.php?posterId=".$tvshow->getPosterId()."' alt='poster de la série'></div>");
+    $webPage->appendContent("<div class='list'>");
+
+    $webPage->appendContent("<div class=serie__pres>");
+    $webPage->appendContent("<div class='serie__image'><img src='poster.php?posterId=".$tvshow->getPosterId()."' alt='poster de la série'></div>");
     $webPage->appendContent("<h3 class='nom_serie'>{$tvshow->getName()}</h3>");
     $webPage->appendContent("<h4 class='nom_original_serie'>{$tvshow->getOriginalName()}</h4>");
     $webPage->appendContent("<p class='overview_serie'>{$tvshow->getOverview()}</p></div>");
 
 
-    $bd = Entity\Collection\SeasonCollection::findBySeasonId();
+    $bd = Entity\Collection\SeasonCollection::findBySeasonId($tvshowId);
 
     foreach ($bd as $season) {
         $poster = $season->getPosterId();
         $name = AppWebPage::escapeString($season->getName());
         $webPage->appendContent(
             <<<HTML
-                <div class='season'>
-                    <div class="poster_serie"><img src="poster.php?posterId={$season->getPosterId()}" alt='poster de la série'></div>
-                    <h3 class='season_name'>{$name}</h3>
+                <div class='serie'>
+                    <div class="serie__image"><img src="poster.php?posterId={$season->getPosterId()}" alt='poster de la série'></div>
+                    <h3 class='serie__txt'>{$name}</h3>
                 </div>
 
 HTML
         );
     }
+    $webPage->appendContent("</div>");
 
     echo $webPage->toHTML();
 } catch (EntityNotFoundException) {

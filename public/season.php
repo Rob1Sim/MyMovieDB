c72dc62ba0af094db0deb874ad511afe5a58a927
lalo0014 <gaspard.lalouette@etudiant.univ-reims.fr>
2022-06-01 12:13:02 +0200

    Modification season.php et TvShowCollection.php

--- public/season.php
+++ public/season.php
@@ -5,39 +5,44 @@ declare(strict_types=1);
 use Entity\Exception\EntityNotFoundException;
 use Html\AppWebPage;
 
-if (isset($_GET['seasonId']) && !empty(($_GET['seasonId'])) && ctype_digit($_GET['seasonId'])) {
-    $seasonId = (int)$_GET['seasonId'];
+if (isset($_GET['tvshowId']) && !empty(($_GET['tvshowId'])) && ctype_digit($_GET['tvshowId'])) {
+    $tvshowId = (int)$_GET['tvshowId'];
 } else {
     header("Location: /index.php ");
     exit;
 }
 
 try {
-    $webpage = new AppWebPage();
+    $webPage = new AppWebPage();
 
-    $season = Entity\Season::findBySeasonId($seasonId);
-    $webpage->setTitle("{$season->getName()}");
+    $tvshow = Entity\Collection\TvShowCollection::findByTvShowId($tvshowId);
+    $string = AppWebPage::escapeString($tvshow->getName());
+    $webPage->setTitle("Serie TV : $string");
 
-    $string = AppWebPage::escapeString($season->getName());
+    $webPage->appendContent("<div class=tvshow>");
+    $webPage->appendContent("<div class='poster_serie'><img src='poster.php?posterId=".$tvshow->getPosterId()."' alt='poster de la série'></div>");
+    $webPage->appendContent("<div class='nom_serie'>{$tvshow->getName()}</div>");
+    $webPage->appendContent("<div class='nom_original_serie'>{$tvshow->getOriginalName()}</div>");
+    $webPage->appendContent("<div class='overview_serie'>{$tvshow->getOverview()}</div></div>");
 
-    $bd = Entity\Collection\TvShowCollection::findAll();
 
-    foreach ($bd as $album) {
-        $year = $album->getYear();
-        $name = AppWebPage::escapeString($album->getName());
-        $webpage->appendContent(
+    $bd = Entity\Collection\SeasonCollection::findBySeasonId();
+
+    foreach ($bd as $season) {
+        $poster = $season->getPosterId();
+        $name = AppWebPage::escapeString($season->getName());
+        $webPage->appendContent(
             <<<HTML
-                <div class='album'>
-                    <img src="http://localhost:8000/cover.php?coverId={$album->getCoverId()}" class="album__cover">
-                    <p class='album__year'>{$year}</p>
-                    <p class='album__name'>{$name}</p>
+                <div class='season'>
+                    <div class="poster_serie"><img src="poster.php?posterId={$season->getPosterId()}" alt='poster de la série'></div>
+                    <h3 class='season_name'>{$name}</h3>
                 </div>
 
 HTML
         );
     }
 
-    echo $webpage->toHTML();
+    echo $webPage->toHTML();
 } catch (EntityNotFoundException) {
     http_response_code(404);
 }
